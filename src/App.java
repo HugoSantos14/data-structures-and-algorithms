5795be1aef8ad168da7fcb98d26d65036ecbf262
HugoSantos14 <hugosantos14000@gmail.com>
2025-04-29 11:46:46 -0300

    Algoritmos de ordenação

--- src/App.java
+++ src/App.java
@@ -1,7 +1,4 @@
-import exercises.oldestBook.Book;
-
-import java.util.HashMap;
-import java.util.Map;
+import algorithms.Sorting;
 
 public class App {
     public static void main(String[] args) {
---
+++ src/algorithms/Sorting.java
@@ -0,0 +1,55 @@
+package algorithms;
+
+public class Sorting {
+
+    public void selectionSort(int[] arr) {
+        int size = arr.length;
+
+        for (int i = 0; i < size - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public void bubbleSort(int[] arr) {
+        int size = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < size - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public void insertionSort(int[] arr) {
+
+    }
+
+    public void print(int [] arr) {
+        for (int x : arr) System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        Sorting s = new Sorting();
+
+        int[] arr = {7, 2, 11, 4, 56, 23, 9, 45, 10, 17};
+
+        s.insertionSort(arr);
+        s.print(arr);
+    }
+}
